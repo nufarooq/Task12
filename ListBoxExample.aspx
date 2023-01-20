9c6b4b5fc8a34ac716a480d5ea320e2814e80822
Tejora <numan.992@gmail.com>
2023-01-20 21:19:31 +0530

    Task commit

---
+++ ListBoxExample.aspx
@@ -0,0 +1,32 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="ListBoxExample.aspx.cs" Inherits="ListBoxExample.ListBoxExample" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <asp:Label ID="label1" Text="Choose one of the items below"
+            Font-Name="Verdana" Font-Size="18pt" runat="server" />
+        <br>
+        <br>
+        <asp:ListBox ID="Store" AutoPostBack="True" Width="200"
+            runat="server" OnSelectedIndexChanged="DisplayImage">
+            <asp:ListItem Value="10000">Mobile</asp:ListItem>
+            <asp:ListItem Value="50000">Laptop</asp:ListItem>
+            <asp:ListItem Value="8000">Watch</asp:ListItem>
+            <asp:ListItem Value="4000">HeadPhones</asp:ListItem>
+        </asp:ListBox>
+        <asp:Image ID="Img" ImageUrl="" AlternateText="" runat="server" />
+        <br>
+        <br>
+        <asp:Button ID="btnCost" Text="Find Cost" OnClick="ShowCost"
+            runat="server" />
+        <br>
+        <br>
+        <asp:Label ID="lblCost" runat="server" />
+    </form>
+</body>
+</html>
---
+++ ListBoxExample.aspx.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ListBoxExample
+{
+    public partial class ListBoxExample : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void ShowCost(object sender, EventArgs e)
+        {
+            if (Store.SelectedIndex > -1)
+            {
+                int i = Store.SelectedIndex;
+                lblCost.Text = "You have selected " + Store.SelectedItem.Text + " and the cost of this item is Rs." + Store.SelectedItem.Value;
+            }
+        }
+
+        protected void DisplayImage(object sender, EventArgs e)
+        {
+            int i = Store.SelectedIndex;
+            if (i == 0)
+            {
+                Img.ImageUrl = "mobile.jpg";
+                Img.AlternateText = "Mobile";
+            }
+            else if (i == 1)
+            {
+                Img.ImageUrl = "laptop.jpg";
+                Img.AlternateText = "Laptop";
+            }
+            else if (i == 2)
+            {
+                Img.ImageUrl = "~/watch.jpg";
+                Img.AlternateText = "Watch";
+            }
+            else
+            {
+                Img.ImageUrl = "~/headphones.jpg";
+                Img.AlternateText = "Head Phones";
+            }
+        }
+    }
+}
